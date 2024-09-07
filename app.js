9fcfcce030c1b7e2ee6c759f271e76647265c841
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-09-07 22:09:12 +0530

    adminsignin created

--- app.js
+++ app.js
@@ -64,6 +64,32 @@ app.post("/signin",(req,res)=>{
     result.save()
     res.json({"status":"success"})
 })
+app.post("/adminsignin",(req,res)=>{
+    let input=req.body
+    let result=loginModel.find({username:input.username}).then(
+        (response)=>{
+            if (response.length>0) {
+                const validator=bcrypt.compareSync(input.password,response[0].password)
+                if(validator)
+                {
+                    jwt.sign({email:input.username},"home-app",{expiresIn:"1d"},
+                    (error,token)=>{
+                        if (error) {
+                            res.json({"status":"Token Creation Failed"})
+                        } else {
+                            res.json({"status":"success","token":token})
+                        }
+                    })
+                }else
+                {
+                    res.json({"status":"Wrong Password"})
+                }
+            } else {
+                res.json({"status":"Invalid Authentication"}) 
+            }
+        }
+    ).catch()
+})
 app.listen(8080,()=>{
     console.log("server started")
 })
